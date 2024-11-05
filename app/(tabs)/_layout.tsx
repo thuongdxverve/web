4b2c71dcce57f0950c4245edfb0e12614c6242f4
thuongdxverve <thuongdx@verve-inc.jp>
2024-11-05 11:54:02 +0900

    first commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: true,
+      }}>
+        <Tabs.Screen
+            name="coin"
+            options={{
+                title: 'COIN',
+                tabBarIcon: ({ color, focused }) => (
+                    <FontAwesome5 name="coins" size={28} color={color} />
+                ),
+            }}
+        />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'HOME',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'EXPLORE',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/coin.tsx
@@ -0,0 +1,45 @@
+import { Stack } from 'expo-router';
+import { Button, Text, Image, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+function LogoTitle() {
+    return (
+        <Image style={styles.image_logo} source={require('@/assets/images/logo.png')} />
+    );
+}
+
+function ImageBell() {
+    return (
+        <Image style={styles.image_bell} source={require('@/assets/images/bell-icon.png')} />
+    );
+}
+
+export default function Home() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <Stack.Screen
+                options={{
+                    headerTitle: props => <LogoTitle {...props} />,
+                    headerTitleAlign: "center",
+                    headerRight: props => <ImageBell {...props} />,
+
+                }}
+            />
+            <Text>Count: {count}</Text>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    image_logo: {
+        width: 238,
+        height: 37,
+    },
+    image_bell: {
+        width: 32,
+        height: 37,
+        marginRight: 10
+    },
+});
