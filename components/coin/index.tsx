ad6a55c58db0ceee542b814db2029f4e4e047248
thuongdxverve <thuongdx@verve-inc.jp>
2024-11-08 11:40:43 +0900

    change error FlatList inside ScrollView (ios)

--- components/coin/index.tsx
+++ components/coin/index.tsx
@@ -9,72 +9,73 @@ import styles from './coin-styles';
 export default function Coin() {
 
   return (
-    <FlatList
-      data={coinHistoryMock}
-      scrollsToTop={true}
-      renderItem={({ item }) => (
-        <ThemedView style={styles.overGive}>
-          <ThemedView style={{ marginBottom: 15 }}>
-            <ThemedView style={styles.sectionGive}>
-              <Text style={styles.titleRespected}>BE RESPECTEDコイン</Text>
-              <ThemedView style={styles.sectionInfoTitle}>
-                <Text style={styles.titleHeadGive}>
-                  {item.sender_name.length > 20 ? item.sender_name.substring(0, 13) + '...' : item.sender_name}
-                </Text>
-                <Image
-                  style={styles.arrow}
-                  source={require('@/assets/images/coin/flowbite.png')}
-                />
-                <Text style={styles.titleHeadGive}>
-                  {item.receiver_name.length > 20 ? item.receiver_name.substring(0, 13) + '...' : item.receiver_name}
-                </Text>
-              </ThemedView>
+      <ThemedView>
+        {
+          coinHistoryMock.map((item, index)=> (
+              <ThemedView key={item.id}  style={styles.overGive}>
+                <ThemedView style={{ marginBottom: 15 }}>
+                  <ThemedView style={styles.sectionGive}>
+                    <Text style={styles.titleRespected}>BE RESPECTEDコイン</Text>
+                    <ThemedView style={styles.sectionInfoTitle}>
+                      <Text style={styles.titleHeadGive}>
+                        {item.sender_name.length > 20 ? item.sender_name.substring(0, 13) + '...' : item.sender_name}
+                      </Text>
+                      <Image
+                          style={styles.arrow}
+                          source={require('@/assets/images/coin/flowbite.png')}
+                      />
+                      <Text style={styles.titleHeadGive}>
+                        {item.receiver_name.length > 20 ? item.receiver_name.substring(0, 13) + '...' : item.receiver_name}
+                      </Text>
+                    </ThemedView>
 
-              <ThemedView style={styles.avatarGive}>
-                <Image
-                  style={styles.avatarUser}
-                  source={item.sender_avatar}
-                />
-                <ThemedView>
-                  <Image
-                    style={styles.coinTypeImage}
-                    source={require('@/assets/images/coin/coin-type-image.png')}
-                  />
+                    <ThemedView style={styles.avatarGive}>
+                      <Image
+                          style={styles.avatarUser}
+                          source={item.sender_avatar}
+                      />
+                      <ThemedView>
+                        <Image
+                            style={styles.coinTypeImage}
+                            source={require('@/assets/images/coin/coin-type-image.png')}
+                        />
+                      </ThemedView>
+                      <Image
+                          style={styles.avatarUser}
+                          source={item.receiver_avatar}
+                      />
+                    </ThemedView>
+                  </ThemedView>
+
+                  <Text style={styles.description}>
+                    {item.comment.length > 50 ? item.comment.substring(0, 60) + '...' : item.comment}
+                  </Text>
+
+                  <ThemedView style={styles.actionComment}>
+                    <Text style={styles.times}>{item.send_time}</Text>
+                    <View style={styles.sectionIcon}>
+                      <View style={styles.sectionHeart}>
+                        <Ionicons name="image-outline" size={24} color="black" />
+                        <Text style={{ marginLeft: 10 }}>45</Text>
+                      </View>
+                      <View style={{ marginLeft: 10 }}>
+                        <Ionicons name="chatbubble-outline" size={24} color="black" />
+                      </View>
+                      <View style={{ marginLeft: 10 }}>
+                        <AntDesign name="minus" size={10} color="black" />
+                      </View>
+                      <View style={styles.sectionHeart}>
+                        <AntDesign name="heart" size={24} color="red" />
+                        <Text style={{ marginLeft: 10 }}>45</Text>
+                      </View>
+                    </View>
+                  </ThemedView>
+
                 </ThemedView>
-                <Image
-                  style={styles.avatarUser}
-                  source={item.receiver_avatar}
-                />
               </ThemedView>
-            </ThemedView>
+          ))
+        }
+      </ThemedView>
 
-            <Text style={styles.description}>
-              {item.comment.length > 50 ? item.comment.substring(0, 60) + '...' : item.comment}
-            </Text>
-
-            <ThemedView style={styles.actionComment}>
-              <Text style={styles.times}>{item.send_time}</Text>
-              <View style={styles.sectionIcon}>
-                <View style={styles.sectionHeart}>
-                  <Ionicons name="image-outline" size={24} color="black" />
-                  <Text style={{ marginLeft: 10 }}>45</Text>
-                </View>
-                <View style={{ marginLeft: 10 }}>
-                  <Ionicons name="chatbubble-outline" size={24} color="black" />
-                </View>
-                <View style={{ marginLeft: 10 }}>
-                  <AntDesign name="minus" size={10} color="black" />
-                </View>
-                <View style={styles.sectionHeart}>
-                  <AntDesign name="heart" size={24} color="red" />
-                  <Text style={{ marginLeft: 10 }}>45</Text>
-                </View>
-              </View>
-            </ThemedView>
-
-          </ThemedView>
-        </ThemedView>
-      )}
-    />
   );
 }
